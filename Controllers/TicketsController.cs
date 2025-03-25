75345300ce543af8a16bb883c8e74ce17f8ad192
HKasheke <hope.kasheke@gmail.com>
2025-03-25 13:45:27 -0300

    initial commit

---
+++ Controllers/TicketsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TIcketHub.Models;
+
+namespace TIcketHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ILogger<TicketsController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public TicketsController(ILogger<TicketsController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from Tickets Controller");
+        }
+
+        [HttpPost]
+        public IActionResult Post(Purchase purchase)
+        {
+            if (string.IsNullOrEmpty(purchase.Name))
+            {
+                return BadRequest("Invalid name");
+            }
+
+            if (string.IsNullOrEmpty(purchase.Email))
+            {
+                return BadRequest("Invalid name");
+            }
+
+            return Ok("Hello from post" + purchase.Name + "Email: " + purchase.Email);
+        }
+    }
+}
