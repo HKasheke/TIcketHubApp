926ee2f05dd0e4a475eca457b39473616f5c2524
HKasheke <hope.kasheke@gmail.com>
2025-04-07 21:58:30 -0300

    cors fixed

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text.Json;
 using Azure.Storage.Queues;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TIcketHub.Models;
 
--- Program.cs
+++ Program.cs
@@ -7,6 +7,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Add user secrets
@@ -23,6 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
